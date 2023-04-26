445c44959c4f46cc5651721db777ef52163a3841
mairac <mairac@kyros.com>
2023-04-25 23:13:04 -0300

    feature: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PaymentPageComponent } from './components/payment-page/payment-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'login', component: LoginComponent },
+{ path: 'payment', component: PaymentPageComponent },
+{ path: '', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
